c58dc40543387dee84b338e512e815e0a2f42a5f
Joao Nadais <joaonadais@gmail.com>
2021-11-11 23:47:30 +0100

    Add health checks

--- api/Karaoke.Api/Program.cs
+++ api/Karaoke.Api/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddMemoryCache();
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseCors(options => options.AllowAnyOrigin());
 
 app.UseAuthorization();
-
+app.MapHealthChecks("/health");
 app.MapControllers();
 
 app.Run();

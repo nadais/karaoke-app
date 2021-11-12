187e9a5c2d76b5f3dcd81440dfc03748b62ce9fb
Joao Nadais <joaonadais@gmail.com>
2021-11-12 18:24:51 +0100

    Add quick filter and improve styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,61 +2,71 @@ import React, { useState, useEffect } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
 import { GridOptions } from 'ag-grid-community';
 
-
 import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
     const storageKey = "karaoke-storage";
     const [rowData, setRowData] = useState(getLocalStorageSongs());
-    var gridOptions = {
-        defaultColDef: {
-          resizable: true,
-        },
-        columnDefs: [
-            { field: 'name', sortable: true, filter: true },
-            { field: 'artist', sortable: true, filter: true },
-          ],
-        rowData: rowData,
-        onColumnResized: function (params) {
-          console.log(params);
-        },
-      };
+    const [gridApi, setGridApi] = useState(null);
+    const [gridColumnApi, setGridColumnApi] = useState(null);
+    const searchDivStyle={backgroundColor:"#dedede",padding:10}
+    const searchStyle={width:"100%",padding:"10px 20px",borderRadius:20,outline:0,
+    border:"2px #68bf40 solid",fontSize:"100%"}
+    
+    const onGridReady = (params) => {
+        setGridApi(params.api);
+        setGridColumnApi(params.columnApi);
+    };
+    const onFilterTextChange=(e)=>{
+      gridApi.setQuickFilter(e.target.value)
+    }    
     useEffect(() => {
         var songs = getLocalStorageSongs();
-        if(songs.Length == 0)
-        {
+        if (songs.length == 0) {
             fetchSongsRemotely();
             return;
         }
         setRowData(songs);
     }, []);
-    function getLocalStorageSongs()
-    {
+    function getLocalStorageSongs() {
         var songs = window.localStorage.getItem(storageKey);
-        if(songs == null)
-        {
+        if (songs == null) {
             return [];
         }
         return JSON.parse(songs);
     }
     function fetchSongsRemotely() {
-        fetch('https://localhost:7232/songs')
+        fetch('http://karaoke-wedding.azurewebsites.net/songs')
             .then(result => {
                 var test = result.json();
                 return test;
             })
-            .then(rows =>
-                {
-                    window.localStorage.setItem(storageKey, JSON.stringify(rows));
-                    setRowData(rows);
-                });
+            .then(rows => {
+                window.localStorage.setItem(storageKey, JSON.stringify(rows));
+                setRowData(rows);
+            });
     }
 
     return (
-        <div className="container ag-theme-balham" style={{ height: 1000, width: 500 }}>
-            <AgGridReact gridOptions={gridOptions} />
+        <div>
+            <h1 align="center">Karaoke night</h1>
+            <div style={searchDivStyle}>
+                <input type="search" style={searchStyle} onChange={onFilterTextChange} placeholder="search songs..." />
+            </div>
+            <div className="ag-theme-alpine" style={{ height: 600 }}>
+                <AgGridReact rowData={rowData}
+                    defaultColDef={{
+                        flex: 1,
+                        sortable: true,
+                        resizable: true,
+                        filter: true,
+                    }}
+                    onGridReady={onGridReady}>
+                    <AgGridColumn field="name" sortable={true} filter={true} />
+                    <AgGridColumn field="artist" sortable={true} filter={true} />
+                </AgGridReact>
+            </div>
         </div>
     );
 };

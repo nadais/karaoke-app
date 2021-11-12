07c783523011ad999d8f53172bb84f4ac8cb7cde
Joao Nadais <joaonadais@gmail.com>
2021-11-12 18:40:08 +0100

    Add loading spinner and reload button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,21 @@ function App() {
     const storageKey = "karaoke-storage";
     const [rowData, setRowData] = useState(getLocalStorageSongs());
     const [gridApi, setGridApi] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [gridColumnApi, setGridColumnApi] = useState(null);
-    const searchDivStyle={backgroundColor:"#dedede",padding:10}
-    const searchStyle={width:"100%",padding:"10px 20px",borderRadius:20,outline:0,
-    border:"2px #68bf40 solid",fontSize:"100%"}
-    
+    const searchDivStyle = { backgroundColor: "#dedede", padding: 10 }
+    const searchStyle = {
+        width: "100%", padding: "10px 20px", borderRadius: 20, outline: 0,
+        border: "2px #68bf40 solid", fontSize: "100%"
+    }
+
     const onGridReady = (params) => {
         setGridApi(params.api);
         setGridColumnApi(params.columnApi);
     };
-    const onFilterTextChange=(e)=>{
-      gridApi.setQuickFilter(e.target.value)
-    }    
+    const onFilterTextChange = (e) => {
+        gridApi.setQuickFilter(e.target.value)
+    }
     useEffect(() => {
         var songs = getLocalStorageSongs();
         if (songs.length == 0) {
@@ -36,13 +39,22 @@ function App() {
         }
         return JSON.parse(songs);
     }
+    function getLoading() {
+        if (loading) {
+            return <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        }
+    }
     function fetchSongsRemotely() {
-        fetch('http://karaoke-wedding.azurewebsites.net/songs')
+        setLoading(true);
+        fetch('https://karaoke-wedding.azurewebsites.net/songs')
             .then(result => {
                 var test = result.json();
                 return test;
             })
             .then(rows => {
+                setLoading(false);
                 window.localStorage.setItem(storageKey, JSON.stringify(rows));
                 setRowData(rows);
             });
@@ -51,9 +63,14 @@ function App() {
     return (
         <div>
             <h1 align="center">Karaoke night</h1>
+            <button onClick={fetchSongsRemotely}>
+                Reload Songs
+            </button>
+            {getLoading()}
             <div style={searchDivStyle}>
                 <input type="search" style={searchStyle} onChange={onFilterTextChange} placeholder="search songs..." />
             </div>
+
             <div className="ag-theme-alpine" style={{ height: 600 }}>
                 <AgGridReact rowData={rowData}
                     defaultColDef={{

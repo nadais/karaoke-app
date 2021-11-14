304ca502c8c7b0773de2e4e6c670ae97dd67b170
Joao Nadais <joaonadais@gmail.com>
2021-11-14 21:47:11 +0100

    Clean up csproj and Program cs

--- api/Karaoke.Api/Program.cs
+++ api/Karaoke.Api/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddMemoryCache();
 builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

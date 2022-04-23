625e0e6370bf32b8aa3cb99c1ef73b2bff9e19e6
Joao Nadais <joaonadais@gmail.com>
2022-04-23 13:31:04 +0200

    Add support for multiple languages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { useTranslation } from 'react-i18next';
+import { BrowserRouter, useParams, useSearchParams } from 'react-router-dom';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
+function Page() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { t, i18n } = useTranslation();
     const [rowData, setRowData] = useState([]);
     const [gridApi, setGridApi] = useState(null);
     const [loading, setLoading] = useState(false);
@@ -26,28 +30,45 @@ function App() {
         gridApi.setQuickFilter(e.target.value)
     }
     useEffect(() => {
+        let lang = searchParams.get('lang');
+        if (lang != null) {
+            i18n.changeLanguage(lang);
+        }
         fetchSongsRemotely();
     }, []);
     function getLoading() {
         if (loading) {
             return <div className="spinner-border" role="status">
-                <span className="visually-hidden">Loading...</span>
+                <span className="visually-hidden"> {t('loading')}</span>
             </div>
         }
     }
     function getCategories() {
         return <div className="col">
             <select className="form-select" name='catalog' onChange={onCategoryChanged}>
-                <option value=""> Select category</option>
+                <option value=""> {t('selectCategory')}</option>
                 {categories.map(key =>
-                    (<option value={key} key={key}>{key}</option>))}
+                    (<option value={key} key={key}>{t(`categories.${key.toLowerCase()}`)}</option>))}
             </select>
         </div>
+    }
+    function getLanguages() {
+        let languages = {
+            "en": "English",
+            "pt": "Português"
+        }
+        return <span>
+            <select name='language' onChange={onLanguageChanged}>
+                <option selected disabled> {t('selectLanguage')}</option>
+                {Object.keys(languages).map(key =>
+                    (<option value={key} key={key}>{languages[key]}</option>))}
+            </select>
+        </span>
     }
     function getSearchBar() {
         if (rowData.length > 0) {
             return <div style={searchDivStyle}>
-                <input type="search" style={searchStyle} onChange={onFilterTextChange} placeholder="search songs..." />
+                <input type="search" style={searchStyle} onChange={onFilterTextChange} placeholder={t('searchSongs')} />
             </div>
         }
     }
@@ -70,12 +91,10 @@ function App() {
             });
     }
 
-    function getRowData(newCatalogName, newCategory)
-    {
+    function getRowData(newCatalogName, newCategory) {
         let result = newCatalogName == null || newCatalogName === '' ? songs : songs.filter(x => x.catalogs.includes(newCatalogName));
-        if(newCategory != null && newCategory !== '')
-        {
-            result = result.filter(x =>x.categories.includes(newCategory));
+        if (newCategory != null && newCategory !== '') {
+            result = result.filter(x => x.categories.includes(newCategory));
         }
         return result;
     }
@@ -86,27 +105,43 @@ function App() {
         setRowData(getRowData(catalogName, newCategory));
     }
 
+    function onLanguageChanged(event) {
+        let lang = event.target.value;
+        let updatedSearchParams = new URLSearchParams(searchParams.toString());
+        updatedSearchParams.set('lang', lang);
+        setSearchParams(updatedSearchParams.toString());
+        i18n.changeLanguage(lang);
+    }
+
     function onCatalogChanged(event) {
         let newCatalog = event.target.value === '' ? undefined : event.target.value;
         setCatalogName(newCatalog);
         setRowData(getRowData(newCatalog, category));
     }
+    function translate(key) {
+        return t(key);
+    }
 
     return (
         <div>
-            <h1 align="center">Karaoke night</h1>
+            <div className='row'>
+                <div className='col'>
+                    <h1 align="center">{t('title')}</h1>
+                </div>
+                {getLanguages()}
+            </div>
             <div className="row">
                 <div className="col">
                     <select className="form-select" name='catalog' onChange={onCatalogChanged}>
-                        <option value=""> Select catalog</option>
+                        <option value="">{t('selectCatalog')}</option>
                         {catalogs.map(key =>
-                            (<option value={key} key={key}>{key}</option>))}
+                            (<option value={key} key={key}>{t(`catalogs.${key.toLowerCase()}`)}</option>))}
                     </select>
                 </div>
                 {getCategories()}
                 <div className="col">
                     <button onClick={fetchSongsRemotely} className="btn btn-primary">
-                        Reload Songs
+                        {t('reload')}
                     </button>
                     {getLoading()}
                 </div>
@@ -114,24 +149,32 @@ function App() {
             {getSearchBar()}
 
 
-            <div className="ag-theme-alpine"  style={{ "height": 1000 }}>
+            <div className="ag-theme-alpine" style={{ "height": 1000 }}>
                 <AgGridReact rowData={rowData}
                     defaultColDef={{
                         sortable: true,
                         resizable: true,
                         filter: true,
-                        suppressMovable: true,
-                        suppressColumnsToolPanel: false
+                        suppressMovable: true
                     }}
+                    localeTextFunc={translate}
                     onGridReady={onGridReady}>
-                    <AgGridColumn field="number" sortable={true} filter={true} flex={1} minWidth={80}/>
-                    <AgGridColumn field="name" sortable={true} filter={true} flex={4} minWidth={250}/>
-                    <AgGridColumn field="artist" sortable={true} filter={true} flex={2} sort={'asc'} minWidth={170}/>
+                    <AgGridColumn field="number" headerName={translate("number")} sortable={true} filter={true} flex={1} minWidth={80} />
+                    <AgGridColumn field="name" headerName={translate("name")} sortable={true} filter={true} flex={4} minWidth={250} />
+                    <AgGridColumn field="artist" headerName={translate("artist")} sortable={true} filter={true} flex={2} sort={'asc'} minWidth={170} />
                 </AgGridReact>
             </div>
         </div>
     );
 };
 
-
-export default App;
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback="loading">
+                <Page />
+            </Suspense>
+        </BrowserRouter>
+    );
+}
